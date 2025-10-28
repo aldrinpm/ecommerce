d98d99b67151a8be0a4b7e5259b4cca2ee1b5d62
aldrinpm <aldrinpm@gmail.com>
2025-10-28 14:52:06 +0800

    Initial commit

---
+++ services/payment/src/main/java/com/ecommerce/ecommerce/notification/NotificationProducer.java
@@ -0,0 +1,28 @@
+package com.ecommerce.ecommerce.notification;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.message.StopReplicaRequestData;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.kafka.support.KafkaHeaders.TOPIC;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationProducer {
+    private final KafkaTemplate<String, PaymentNotificationRequest> kafkaTemplate;
+
+    public void sendNotification(PaymentNotificationRequest notificationRequest) {
+        log.info("Sending payment notification for order: {}", notificationRequest);
+        Message<PaymentNotificationRequest> message = MessageBuilder
+                .withPayload(notificationRequest)
+                .setHeader(TOPIC, "payment-topic")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
---
+++ services/payment/src/main/java/com/ecommerce/ecommerce/payment/PaymentService.java
@@ -0,0 +1,32 @@
+package com.ecommerce.ecommerce.payment;
+
+import com.ecommerce.ecommerce.notification.NotificationProducer;
+import com.ecommerce.ecommerce.notification.PaymentNotificationRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final PaymentRepository repository;
+    private final PaymentMapper mapper;
+    private NotificationProducer notificationProducer;
+
+    public Integer createPayment(PaymentRequest paymentRequest) {
+        var payment = repository.save(mapper.toPayment(paymentRequest));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        paymentRequest.orderReference(),
+                        paymentRequest.amount(),
+                        paymentRequest.paymentMethod(),
+                        paymentRequest.customer().firstName(),
+                        paymentRequest.customer().lastName(),
+                        paymentRequest.customer().email()
+                )
+        );
+
+        return null;
+    }
+}
