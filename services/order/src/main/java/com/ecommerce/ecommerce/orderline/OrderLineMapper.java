91a961a815b372cb305c660085cbbc3367c73bcb
aldrinpm <aldrinpm@gmail.com>
2025-10-28 09:20:55 +0800

    Initial commit

---
+++ services/order/src/main/java/com/ecommerce/ecommerce/orderline/OrderLineMapper.java
@@ -0,0 +1,18 @@
+package com.ecommerce.ecommerce.orderline;
+
+import com.ecommerce.ecommerce.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLineMapper {
+    public OrderLine toOrderLine(OrderLineRequest orderLineRequest) {
+        return OrderLine.builder()
+                .id(orderLineRequest.id())
+                .quantity(orderLineRequest.quantity())
+                .order(Order.builder()
+                        .id(orderLineRequest.orderId())
+                                .build())
+                .productId(orderLineRequest.productId())
+                .build();
+    }
+}
--- services/order/src/main/java/com/ecommerce/ecommerce/orderline/OrderLineService.java
+++ services/order/src/main/java/com/ecommerce/ecommerce/orderline/OrderLineService.java
@@ -6,6 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class OrderLineService {
-    public void saveOrderLine(OrderLineRequest orderLineRequest) {
+    private final OrderLineRepository orderLineRepository;
+    private final OrderLineMapper orderLineMapper;
+
+    public Integer saveOrderLine(OrderLineRequest orderLineRequest) {
+        var order = orderLineMapper.toOrderLine(orderLineRequest);
+        return orderLineRepository.save(order).getId();
     }
 }
